236f8939ae7bcf1b9c48aad02a02b40c4d03dd0f
Tahmid <you@example.com>
2021-05-13 22:19:36 +0600

    Eid mubarak

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.from(".eid", { duration: 2, opacity: 0 });
+gsap.from(".c", { duration: 1, x:-100 ,delay:0});
+gsap.from(".r", { duration: 1, x: 100 ,delay:1});
+gsap.from(".t", { duration: 1, y: -100 ,delay:0.5});
+gsap.from(".s", { duration: 1, opacity: 0, delay: 1.5 });
+
+gsap.from(".v1", { duration: 0.5, opacity:0,delay:1});
+gsap.from(".v2", { duration: 0.5, opacity: 0, delay: 1.3 });
+gsap.from(".v3", { duration: 0.5, opacity: 0, delay: 1.6 });
+gsap.from(".v4", { duration: 0.5, opacity: 0, delay: 1.9 });
+gsap.from(".v5", { duration: 0.5, opacity: 0, delay: 2.1 });
+gsap.from(".v6", { duration: 0.5, opacity:0,delay:2.4});
+
+
+
+
+
+
+
